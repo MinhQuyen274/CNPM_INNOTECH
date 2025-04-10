01e0a6a153233d3c2e57b65170190bac8feada98
LeThaiVinh <hsthptbb.ltvinh12a11@gmail.com>
2025-04-10 10:36:59 +0700

    Viet ham Xử lý logic tính tổng doanh thu

---
+++ CNPT_INNTECH_QLQUAN_AN/CNPT_INNTECH_QLQUAN_AN/Models/LogicTinhToanUS37.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNPT_INNTECH_QLQUAN_AN.Models
+{
+    class LogicTinhToanUS37
+    {
+    }
+}
