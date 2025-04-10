a5e6ba474273bbe6659684401a26197c65bf9c58
MinhQuyen274 <info.huynhquyen@gmail.com>
2025-04-10 09:22:27 +0700

    Tạo project chung

---
+++ CNPT_INNTECH_QLQUAN_AN/CNPT_INNTECH_QLQUAN_AN/Views/Loginform.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CNPT_INNTECH_QLQUAN_AN.Views
+{
+    public partial class Loginform : Form
+    {
+        public Loginform()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
